1dde82676a9ff1ca327389263a90b64d755518a1
乾 <guy7273@live.com>
2018-01-06 22:45:25 +0800

    Section1Chapter5

---
+++ Bobblehead Wars/Assets/Scripts/Alien.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Alien : MonoBehaviour
+{
+    public Transform target;
+    public float navigationUpdate;
+
+    private float navigationTime = 0;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (target != null)
+        {
+            navigationTime += Time.deltaTime;
+            if (navigationTime > navigationUpdate)
+            {
+                agent.destination = target.position;
+                navigationTime = 0;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
