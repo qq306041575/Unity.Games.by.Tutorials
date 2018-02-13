67e5d591448dc5338bb36f696dbeb58ad69d2d0e
乾 <guy7273@live.com>
2018-02-13 21:44:58 +0800

    Section1Chapter6

---
+++ Bobblehead Wars/Assets/Scripts/ArenaWall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaWall : MonoBehaviour
+{
+    private Animator arenaAnimator;
+
+    void Start()
+    {
+        GameObject arena = transform.parent.gameObject;
+        arenaAnimator = arena.GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        arenaAnimator.SetBool("IsLowered", true);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        arenaAnimator.SetBool("IsLowered", false);
+    }
+}
--- Bobblehead Wars/Assets/Scripts/PlayerController.cs
+++ Bobblehead Wars/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 50.0f;
     public Rigidbody head;
     public LayerMask layerMask;
+    public Animator bodyAnimator;
 
     private Vector3 currentLookTarget = Vector3.zero;
     private CharacterController characterController;
@@ -27,11 +28,12 @@ public class PlayerController : MonoBehaviour
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         if (moveDirection == Vector3.zero)
         {
-            // TODO
+            bodyAnimator.SetBool("IsMoving", false);
         }
         else
         {
             head.AddForce(transform.right * 150, ForceMode.Acceleration);
+            bodyAnimator.SetBool("IsMoving", true);
         }
 
         RaycastHit hit;
