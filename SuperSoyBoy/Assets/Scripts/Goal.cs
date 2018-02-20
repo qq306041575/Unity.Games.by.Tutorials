0a8b900d9d0f4e40771ff7223f5d819ed1477455
乾 <guy7273@live.com>
2018-02-20 22:43:14 +0800

    Section3Chapter13

---
+++ SuperSoyBoy/Assets/Scripts/Goal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    public AudioClip goalClip;
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            var audioSource = GetComponent<AudioSource>();
+            if (audioSource != null && goalClip != null)
+            {
+                audioSource.PlayOneShot(goalClip);
+            }
+            GameManager.instance.RestartLevel(0.5f);
+        }
+    }
+}
